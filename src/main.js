00bb80eac5d72cedaee1b5af53b09d8e2d782fcd
gracias <gracias.claude@outlook.com>
2023-07-10 07:10:37 -0600

    gc/vue3 Missing Package / configs - Add package.json for the default vite scripts - Add `tsconfig, tailwindcss, autoprefixer, postcss`

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import App from "./App.vue";
+import { createStore } from "vuex";
+import AthleteData from "@/assets/data.json";
+import './main.css'
+
+Vue.use(Vuex);
+Vue.config.productionTip = false;
+
+
+export const store = createStore({
+  state() {
+    return {
+      altheticInfo: { ...AthleteData }
+    };
+  },
+  mutations: {
+    updateAlthlesName(state, value) {
+      // update the althetes name here
+      state.name = value
+    }
+  },
+  actions: {
+    updateAlthlesName(context) {
+      context.commit('updateAlthlesName')
+    }
+  }
+});
+
+// Install the store instance as a plugin
+new Vue({
+  el: "#app",
+  store,
+  // Binds to the #app element in public/index.html; Only works with this ID within codesandbox
+  render: (h) => h(App)
+});
